59cabc08041bd6171e8262e325581c740bfd5eab
Chris Owens <guitarrockstarco@gmail.com>
2020-09-11 19:04:41 -0400

    Complete http-client.js

--- http-client.js
+++ http-client.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const http = require('http');
 
 const url = process.argv[2];
@@ -6,5 +8,5 @@ http.get(url, (res) => {
     res.setEncoding('utf-8');
     res.on('data', (data) => {
         console.log(data);
-    })
+    });
 })
